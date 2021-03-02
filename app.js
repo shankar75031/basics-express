2f14bb121a054915d4df7eb5abcc9122470fa4fa
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-03-03 01:36:36 +0530

    added routes

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
 const app = express();
 
 // app.use((req, res, next) => {
@@ -9,20 +12,7 @@ const app = express();
 // });
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    "<form action='/product' method='POST'><input type='text' name='title'/> <button type='submit'>Send</button></form>"
-  );
-});
-
-app.post("/product", (req, res) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello from express</h1>");
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
